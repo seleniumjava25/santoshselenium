d4b8f03de9dff91892ffd697bcde0b2eb3e0229a
samira <samiradahmane29@gmail.com>
2025-12-09 02:06:32 -0500

    change the timeout to 15

--- santoshselenium/src/main/java/authonticationAlert.java
+++ santoshselenium/src/main/java/authonticationAlert.java
@@ -7,7 +7,7 @@ public class authonticationAlert {
 
 	public static void main(String[] args) {
 		WebDriver driver =new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 //admin:admin@(url)	
 		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
 		driver.manage().window().maximize();
